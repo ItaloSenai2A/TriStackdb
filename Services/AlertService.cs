45bfb745f292463daea904ad644e3efff68d657c
Ítalo <italooo.senai@gmail.com>
2025-11-11 08:33:18 -0300

    Desativa importações não utilizadas em AlertService.cs
    Comentários foram adicionados às linhas de importação dos namespaces `TriStack.Data` e `TriStack.Models` no arquivo `AlertService.cs`, desativando essas importações. Isso ajuda a manter o código mais limpo e evita dependências desnecessárias.

--- Services/AlertService.cs
+++ Services/AlertService.cs
@@ -2,8 +2,8 @@
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using TriStack.Data;
-using TriStack.Models;
+//using TriStack.Data;
+//using TriStack.Models;
 
 namespace TriStack.Services
 {
